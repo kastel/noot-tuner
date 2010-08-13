9b37611e4c46f6cad43ee4081241ed3eaf414c34
dc kastel <dc.kastel@gmail.com>
2010-08-13 02:51:19 +0200

    Spec files improvement

--- gub/gub/specs/noot-tuner.py
+++ gub/gub/specs/noot-tuner.py
@@ -4,5 +4,6 @@ from gub import tools
 class Noot_tuner(target.AutoBuild):
 	source = 'http://dl.dropbox.com/u/8231147/noot-tuner-0.1.0.1.tar.gz'
 	dependencies = ['portaudio', 'libfftw', 'wxwidgets']
-        configure_flags = (tools.AutoBuild.configure_flags +
-          ' --with-wx-config="%(targetdir)s/root/%(prefix_dir)s/bin/wx-config --prefix=%(targetdir)s/root/%(prefix_dir)s"')
+	#how to avoid the following line?
+        configure_flags = (target.AutoBuild.configure_flags +
+          ' --with-wx-config=%(targetdir)s/root/%(prefix_dir)s/bin/wx-config-replacement')
--- gub/gub/specs/wxwidgets.py
+++ gub/gub/specs/wxwidgets.py
@@ -6,9 +6,9 @@ from gub import context
 
 class Wxwidgets(target.AutoBuild):
 	source = 'https://downloads.sourceforge.net/project/wxwindows/2.8.11/wxWidgets-2.8.11.tar.gz?r=http%%3A%%2F%%2Fwxwidgets.org%%2Fdownloads%%2F&ts=1281469491&mirror=heanet'
-	configure_flags = tools.AutoBuild.configure_flags + ' --without-subdirs --without-gnomeprint PKG_CONFIG=%(targetdir)s/pkg-config-replacement'
+	configure_flags = target.AutoBuild.configure_flags + ' --without-subdirs --without-gnomeprint PKG_CONFIG=%(targetdir)s/pkg-config-replacement'
 	dependencies = ['libtiff','libpng','gettext']
-	def configure(self):
+	def patch(self): #Create a fake pkg-config which prefixes all include and library paths with the target directory
 		fake_pkgc_path = '%(targetdir)s/pkg-config-replacement' % self.get_substitution_dict()
 		os.system('''
 echo "#!/bin/sh
@@ -17,12 +17,27 @@ export PKG_CONFIG_PATH=%(targetdir)s/root/%(prefix_dir)s/lib/pkgconfig:%(targetd
 
 chmod +x %(targetdir)s/pkg-config-replacement
 ''' % self.get_substitution_dict())
-		target.AutoBuild.configure(self)
+		target.AutoBuild.patch(self)
+	def install(self):
+		target.AutoBuild.install(self)
+		#Create a fake wx-config which prefixes all include and library paths with the target directory
+		#(use this as WXCONFIG in programs which use wxWidgets)
+		os.system(
+'''
+echo "#!/bin/sh
+%(targetdir)s/root/%(prefix_dir)s/bin/wx-config \$* | sed 's#-\([IL]\)/#-\\\\\\1%(targetdir)s/root/#g'" > %(targetdir)s/root/%(prefix_dir)s/bin/wx-config-replacement
 
-#class Wxwidgets__linux(Wxwidgets):
-#	configure_flags = tools.AutoBuild.configure_flags + '--with-x11 --without-subdirs'
-#	dependencies = Wxwidgets.dependencies + ['libsm-devel', 'libx11-devel', 'libxinerama-devel'] #['gtk+-devel', 'tools::glib']
+chmod +x %(targetdir)s/root/%(prefix_dir)s/bin/wx-config-replacement
+''' % self.get_substitution_dict())
 
+#First version: wxGTK (uncomment which you prefer)
 class Wxwidgets__linux(Wxwidgets):
-	dependencies = Wxwidgets.dependencies + ['gtk+-devel', 'tools::glib']
+	configure_flags = Wxwidgets.configure_flags + ' --with-x11 --enable-unicode'
+	dependencies = Wxwidgets.dependencies + ['libsm-devel', 'libx11-devel', 'libxinerama-devel']
+
+#Second version: wxX11
+#class Wxwidgets__linux(Wxwidgets):
+#	configure_flags = Wxwidgets.configure_flags + ' --without-x11'
+#	dependencies = Wxwidgets.dependencies + ['gtk+-devel', 'tools::glib', 'libxinerama']
+
 
