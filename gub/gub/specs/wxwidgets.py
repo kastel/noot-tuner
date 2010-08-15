3c9ec7bc8e255d07d659492f757fdc6c0e8b93f9
dc kastel <dc.kastel@gmail.com>
2010-08-16 00:08:28 +0200

    Noot-tuner now works well with GUB

--- gub/gub/specs/wxwidgets.py
+++ gub/gub/specs/wxwidgets.py
@@ -5,10 +5,11 @@ from gub import tools
 from gub import context
 
 class Wxwidgets(target.AutoBuild):
-	source = 'https://downloads.sourceforge.net/project/wxwindows/wxAll/2.9.0/wxWidgets-2.9.0.tar.gz?r=http%%3A%%2F%%2Fwxwidgets.org%%2Fdownloads%%2F&ts=1281666329&mirror=sunet'
-	#source = 'https://downloads.sourceforge.net/project/wxwindows/2.8.11/wxWidgets-2.8.11.tar.gz?r=http%%3A%%2F%%2Fwxwidgets.org%%2Fdownloads%%2F&ts=1281469491&mirror=heanet'
-	configure_flags = target.AutoBuild.configure_flags + ' --without-subdirs --without-gnomeprint PKG_CONFIG=%(targetdir)s/pkg-config-replacement'
+	#source = 'https://downloads.sourceforge.net/project/wxwindows/wxAll/2.9.0/wxWidgets-2.9.0.tar.gz?r=http%%3A%%2F%%2Fwxwidgets.org%%2Fdownloads%%2F&ts=1281666329&mirror=sunet'
+	source = 'https://downloads.sourceforge.net/project/wxwindows/2.8.11/wxWidgets-2.8.11.tar.gz?r=http%%3A%%2F%%2Fwxwidgets.org%%2Fdownloads%%2F&ts=1281469491&mirror=heanet'
+	configure_flags = target.AutoBuild.configure_flags + ' --enable-unicode --enable-threads --without-subdirs --without-gnomeprint PKG_CONFIG=%(targetdir)s/pkg-config-replacement'
 	dependencies = ['libtiff','libpng','gettext']
+	patches = ['wxwidgets-2.8.11-spinctlg-1.patch']
 	def patch(self): #Create a fake pkg-config which prefixes all include and library paths with the target directory
 		fake_pkgc_path = '%(targetdir)s/pkg-config-replacement' % self.get_substitution_dict()
 		os.system('''
@@ -33,12 +34,11 @@ chmod +x %(targetdir)s/root/%(prefix_dir)s/bin/wx-config-replacement
 
 #First version: wxGTK (uncomment which you prefer)
 class Wxwidgets__linux(Wxwidgets):
-	configure_flags = Wxwidgets.configure_flags + ' --with-x11 --enable-unicode'
+	configure_flags = Wxwidgets.configure_flags + ' --with-x11'
 	dependencies = Wxwidgets.dependencies + ['libsm-devel', 'libx11-devel', 'libxinerama-devel', 'pangoxft-devel']
 
 #Second version: wxX11
 #class Wxwidgets__linux(Wxwidgets):
 #	configure_flags = Wxwidgets.configure_flags + ' --without-x11'
 #	dependencies = Wxwidgets.dependencies + ['gtk+-devel', 'tools::glib', 'libxinerama']
-
 
