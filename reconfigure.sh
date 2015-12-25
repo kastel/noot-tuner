8e3433baa97b3221973a41d9a616ab5376ede1f8
kastel <dc.kastel@gmail.com>
2015-12-25 03:12:40 +0100

    Cleaned up all generated files (included files generated by gettextize)

--- reconfigure.sh
+++ reconfigure.sh
@@ -3,7 +3,12 @@
 # Call this script after downloading the source from a SCM
 # (svn, Mercurial, git, ...)
 
-aclocal
+touch ChangeLog
 libtoolize -i
+gettextize --no-changelog -f
+mv -f Makefile.am~ Makefile.am
+mv -f configure.ac~ configure.ac
+rm po/Makevars.template
+aclocal
 autoconf
 automake --add-missing
